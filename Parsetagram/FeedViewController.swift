9cc28a624eadaf149ecedac064bd6cd58021b196
Kennedi Cosma <kencosma2020@outlook.com>
2021-08-16 01:01:00 -0400

    This is Parsetagram

---
+++ Parsetagram/FeedViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FeedViewController.swift
+//  Parsetagram
+//
+//  Created by Kennedi Cosma on 8/15/21.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Parsetagram/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  Parsetagram
+//
+//  Created by Kennedi Cosma on 8/16/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
