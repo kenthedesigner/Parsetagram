36e5c41f5192d0826c7011d9d709d32f6615b06b
Kennedi Cosma <kencosma2020@outlook.com>
2021-08-15 21:38:09 -0400

    Initial Commit

---
+++ Parsetagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parsetagram
+//
+//  Created by Kennedi Cosma on 8/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
